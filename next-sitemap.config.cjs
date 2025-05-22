f2e23270e85ad8465e566304154d1a571368d82b
prashant-s29 <prashant.s2922@gmail.com>
2025-05-22 22:50:25 +0530

    added next-sitemap and metadata

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://canvas-alpha-ten.vercel.app",
+  generateRobotsTxt: true,
+  exclude: ["/api*"],
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,18 @@
-// TODO: - Update the siteConfig with your own values
-
-import type { SiteConfig } from '~/types';
+import type { SiteConfig } from "~/types";
 
 export const siteConfig: SiteConfig = {
-  name: 'Full Stack T3 Template',
-  description: 'A better way to scaffold your fullstack application',
-  url: 'https://prashantsingh.me',
-  domain: 'prashantsingh.me',
+  name: "Canvas",
+  description:
+    "Introducing Canvas - A fully managed certificate generator. With Canvas, you can easily generate, verify and manage certificates for your online courses, projects, and more",
+  url: "https://canvas-alpha-ten.vercel.app",
+  domain: "canvas-alpha-ten.vercel.app",
   ogImage: {
-    url: 'https://example.com/images/banner.png',
+    url: "https://9jb1v1y2ig.ufs.sh/f/091oe9IncF3wPbpanIgO5aLfZr7QDjCKs39pentxUyHg0mdR",
     width: 641,
     height: 321,
   },
-  twitterHandle: '@httpsPrashant',
+  twitterHandle: "@prashant_gigs",
   links: {
-    twitter: 'https://twitter.com/httpsPrashant',
+    twitter: "https://x.com/@prashant_gigs",
   },
 };
